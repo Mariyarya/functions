9298b8ada856408c71f2147484dde160307fc78d
Mariyarya <78618476+Mariyarya@users.noreply.github.com>
2021-07-05 14:28:58 +0300

    functions

---
+++ javas.js
@@ -0,0 +1,60 @@
+
+
+
+
+//---------------------------------------------------1--------------------------
+// let kelvinToFahrenheit = (kelvin) => Math.floor(((kelvin - 273.15) * 1.8) + 32);
+// const fahrenheit = kelvinToFahrenheit(293);
+// console.log(`My temp in fahrenheit is ${fahrenheit} degrees `);
+
+//----------------------------------------------------2----------------------
+// let humanAgeInDogYears = (myAge) => (((myAge - 2) * 4) + 21);
+// const dogYears = humanAgeInDogYears(27);
+// console.log(`corresponding number of dog years are ${dogYears}`)
+
+// //-----------------------------------------------3-------------------------------
+// const answerMyQuestion = (userName, userQuestion);
+// if (userName && userName.length) {
+//     console.log(`Hello, ${userName}`)
+// } else {
+//     console.log(`Hello`);
+// }
+// console.log(userQuestion);
+
+// const randomNumber = Math.floor(Math.random() * 8);
+// let eightBall = '';
+// switch (randomNumber) {
+//     case 0:
+//         eightBall = 'It is certain';
+//         brake;
+//     case 1:
+//         eightBall = 'It is decidedly so';
+//         brake;
+//     case 2:
+//         eightBall = 'Reply hazy try again';
+//         brake;
+//     case 3:
+//         eightBall = 'Cannot predict now';
+//         brake;
+//     case 4:
+//         eightBall = 'Do not count on it';
+//         brake;
+//     case 5:
+//         eightBall = 'My sources say no';
+//         brake;
+//     case 6:
+//         eightBall = 'Outlook not so good';
+//         brake;
+//     case 7:
+//         eightBall = 'Signs point to yes';
+//         brake;
+// };
+
+// console.log(`Answer is: ${eightBall}`);
+// console.log(eightBall);
+
+
+//--------------------------------4-------------------------------
+// let raceTime = (userAge, registeredEarly) => nu reusesc sa inteleg cu sa le leg
+// raceTime();
+
